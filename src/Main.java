d479ede271148807793d016d0967b47ac2ae584d
shivakumar.y <shivakumar.y@perfios.com>
2022-05-26 19:49:23 +0530

    Modified : To modified Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import logic.FibonacciSeries;
+import logic.PerfectNumber;
+import logic.PrimeNumber;
+import logic.ReverseNumber;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        StringBuilder sb = new StringBuilder();
+        long startTime = System.nanoTime();
+
+        new FibonacciSeries().FibonacciSeriesOf(args);
+//        new PerfectNumber().perfectNumberOf(args);
+//        new PrimeNumber().PrimeNumberOf(args);
+//        new ReverseNumber().reverseNumberOf(args);
+
+        long stopTime = System.nanoTime();
+        sb.append((stopTime - startTime)/1000000).append(" MS");
+        System.out.println(sb);
     }
 }
