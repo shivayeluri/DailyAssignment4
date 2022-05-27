ebd33927e8108b6fc5bacca3bf96db1c19efc3d9
shivakumar.y <shivakumar.y@perfios.com>
2022-05-27 18:14:31 +0530

    Modified : To modifying Distinct coupon code

--- src/logic/DistinctCouponNumber.java
+++ src/logic/DistinctCouponNumber.java
@@ -1,2 +1,20 @@
-package logic;public class DistinctCouponNumber {
+package logic;
+
+import java.util.HashSet;
+
+public class DistinctCouponNumber {
+
+    public void couponNumber(String[] args) {
+        HashSet<Integer> couponSet = new HashSet<>();
+
+        while(couponSet.size() != Integer.parseInt(args[0])) {
+            int coupon = generateNumber(Integer.parseInt(args[0]));
+            couponSet.add(coupon);
+        }
+        System.out.println(couponSet);
+    }
+
+    public static int generateNumber(int numberOfCoupons) {
+        return (int) (Math.random() * numberOfCoupons) + 1;
+    }
 }
